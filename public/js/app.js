4a2b86bed087ac7d883ba0a767d139a14ff2579e
Wirili <332477694@qq.com>
2016-05-07 00:28:22 +0800

    a

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,25 @@
  */
 $(function(){
     $('.menu-list > a').on('click',function(){
+
         if($(this).parent('.menu-list').hasClass('nav-active')) {
             $(this).parent('.menu-list').removeClass('nav-active')
             $(this).next('.sub-menu-list').hide();
         }else{
+            visibleSubMenuClose();
             $(this).parent('.menu-list').addClass('nav-active')
             $(this).next('.sub-menu-list').show();
         }
     });
+
+    function visibleSubMenuClose() {
+        $('.menu-list').each(function() {
+            var t = $(this);
+            if(t.hasClass('nav-active')) {
+                t.find('> ul').slideUp(200, function(){
+                    t.removeClass('nav-active');
+                });
+            }
+        });
+    }
 });

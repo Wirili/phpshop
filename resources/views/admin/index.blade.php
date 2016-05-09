df848178c4addc572d5e5aaad53ceaee4e66d680
Tms <332477694@qq.com>
2016-05-09 17:55:43 +0800

    auth

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -52,10 +52,10 @@
             </ul>
         </li>
         <li class="menu-list">
-            <a href="javascript:void(0);"><i class="fa fa-home"></i><span>商品管理</span><b class="fa fa-angle-down"></b></a>
+            <a href="javascript:void(0);"><i class="fa fa-home"></i><span>文章管理</span><b class="fa fa-angle-down"></b></a>
             <ul class="sub-menu-list">
-                <li><a href="" target="mainframe">添加商品</a></li>
-                <li><a href="" target="mainframe">添加商品</a></li>
+                <li><a href="" target="mainframe">添加文章</a></li>
+                <li><a href="" target="mainframe">文章类别</a></li>
                 <li><a href="" target="mainframe">添加商品</a></li>
                 <li><a href="" target="mainframe">添加商品</a></li>
             </ul>
